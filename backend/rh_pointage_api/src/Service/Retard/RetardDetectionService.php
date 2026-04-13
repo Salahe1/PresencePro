a9e34a5a05745b650487a0041dbd8d6031249a64
Salahe1 <121726199+Salahe1@users.noreply.github.com>
2026-04-13 21:44:36 +0100

    fix(pointage): correct lateness detection for subsequent entry scans

--- backend/rh_pointage_api/src/Service/Retard/RetardDetectionService.php
+++ backend/rh_pointage_api/src/Service/Retard/RetardDetectionService.php
@@ -14,6 +14,14 @@ class RetardDetectionService
 
         $employe = $pointage->getEmploye();
 
+        $employePointages= $employe->getPointages();
+
+        $AujourdhuiPointages = $employePointages->filter(function (Pointage $p) use ($pointage) {
+            return $p->getTimeStamp() && $p->getTimeStamp()->format('Y-m-d') === $pointage->getTimeStamp()->format('Y-m-d');
+        });
+
+
+
         $departement = $employe->getDepartement();
         if (!$departement) { return null; }
 
@@ -36,6 +44,7 @@ class RetardDetectionService
         $scanAt = $pointage->getTimeStamp();
         if (!$scanAt) {  return null; }
 
+
         $debutPremierePlage = new \DateTimeImmutable(
             $scanAt->format('Y-m-d') . ' ' . $premierePlage->getHeureDebut()->format('H:i:s')
         );
@@ -48,6 +57,11 @@ class RetardDetectionService
 
         if ($scanAt >= $debutPremierePlage && $scanAt <= $finPremierePlage) {
             $heurePrevue = $debutPremierePlage;
+
+            $verificationPremierEntre = $this->VerifierPremierEntre($AujourdhuiPointages, $heurePrevue, $finPremierePlage);
+             if($verificationPremierEntre){
+                  return null;
+                 }
         } elseif ($deuxiemePlage) {
             $debutDeuxiemePlage = new \DateTimeImmutable(
                 $scanAt->format('Y-m-d') . ' ' . $deuxiemePlage->getHeureDebut()->format('H:i:s')
@@ -59,6 +73,11 @@ class RetardDetectionService
 
             if ($scanAt >= $debutDeuxiemePlage && $scanAt <= $finDeuxiemePlage) {
                 $heurePrevue = $debutDeuxiemePlage;
+
+                $verificationPremierEntre = $this->VerifierPremierEntre($AujourdhuiPointages, $heurePrevue, $finDeuxiemePlage);
+                if($verificationPremierEntre){
+                    return null;
+                }
             }
         }
 
@@ -84,5 +103,14 @@ class RetardDetectionService
         $retard->setEmploye($employe);
 
         return $retard;
+    }
+
+    private function VerifierPremierEntre($collection, $debutH, $finH){
+       $dejaPointe= $collection->exists(
+            fn($_ ,Pointage $p) =>
+           $p->getType()?->value === 'entre'  && $p->getTimeStamp() >= $debutH && $p->getTimeStamp() <=$finH
+        );
+    
+        return $dejaPointe;
     }
 }
